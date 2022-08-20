193ca623e6d473811af630988a1fc62b5d05e978
Yanis Boudiaf <yansam2003@gmail.com>
2022-08-20 18:50:18 +0200

    Refactor display list render

--- src/main/java/fr/dynamx/client/renders/model/renderer/ObjObjectRenderer.java
+++ src/main/java/fr/dynamx/client/renders/model/renderer/ObjObjectRenderer.java
@@ -1,12 +1,13 @@
 package fr.dynamx.client.renders.model.renderer;
 
+import com.jme3.math.Vector3f;
 import fr.aym.acslib.api.services.ErrorTrackingService;
-import fr.dynamx.common.DynamXContext;
-import fr.dynamx.common.objloader.Material;
-import fr.dynamx.common.objloader.data.ObjObjectData;
-import fr.dynamx.common.objloader.Vertex;
 import fr.dynamx.client.renders.model.texture.MaterialTexture;
 import fr.dynamx.client.renders.model.texture.TextureData;
+import fr.dynamx.common.DynamXContext;
+import fr.dynamx.common.objloader.Material;
+import fr.dynamx.common.objloader.Vertex;
+import fr.dynamx.common.objloader.data.ObjObjectData;
 import fr.dynamx.utils.DynamXLoadingTasks;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
@@ -16,7 +17,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import org.lwjgl.opengl.GL11;
 
-import javax.vecmath.Vector3f;
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,24 +26,6 @@ import static fr.dynamx.common.DynamXMain.log;
 public class ObjObjectRenderer {
     // Use THIS instead of GlStateManager, it has weird issues due to last bind texture memory and display lists
     private static int bindTexture;
-
-    /**
-     * Binds the texture, if not already bound
-     */
-    private static void bindTexture(int id) {
-        if (id != bindTexture) {
-            GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
-            bindTexture = id;
-        }
-    }
-
-    /**
-     * Resets memory of last bind texture
-     */
-    private static void startDrawing() {
-        bindTexture = -1;
-    }
-
 
     private final Map<Byte, Integer> modelDisplayList = new HashMap<>();
 
@@ -63,6 +46,19 @@ public class ObjObjectRenderer {
         }
     }
 
+    private void compileModel(ObjModelRenderer model, @Nullable TextureData useDefault, @Nullable TextureData variantTextureData) {
+        // Create an empty display list
+        int id = GlStateManager.glGenLists(1);
+        // Start the compilation of the list, this will fill the list with every vertex rendered onwards
+        GlStateManager.glNewList(id, GL11.GL_COMPILE);
+        //Do immediate render
+        renderCPU(model, useDefault != null ? useDefault.getName() : "Default", variantTextureData != null ? variantTextureData.getName() : "Default");
+        // Finish the compilation process
+        GlStateManager.glEndList();
+        modelDisplayList.put(variantTextureData != null ? variantTextureData.getId() : 0, id);
+
+    }
+
     public void createList(TextureData useDefault, TextureData textureData, ObjModelRenderer model, boolean logIfNotFound) {
         if (!isMaterialValid(model, objObjectData.getMesh().materialForEachVertex[0]))
             return;
@@ -76,16 +72,7 @@ public class ObjObjectRenderer {
             }
         }
         if (isCustom) {
-            // Create an empty display list
-            int id = GlStateManager.glGenLists(1);
-            // Start the compilation of the list, this will fill the list with every vertex rendered onwards
-            GlStateManager.glNewList(id, GL11.GL_COMPILE);
-            //Do render
-            renderCPU(model, useDefault.getName(), textureData.getName());
-            // Finish the compilation process
-            GlStateManager.glEndList();
-            modelDisplayList.put(textureData.getId(), id);
-            //log.info("Compile list for "+textureData+" (default is "+useDefault+") for part "+getName()+" of "+model.getLocation());
+            compileModel(model, useDefault, textureData);
         } else {
             if (logIfNotFound)
                 log.error("Failed to find custom texture for skin " + textureData.getName() + " of " + model.getLocation() + " in part " + objObjectData.getName());
@@ -94,18 +81,7 @@ public class ObjObjectRenderer {
     }
 
     public void createDefaultList(ObjModelRenderer model) {
-        if (!isMaterialValid(model, objObjectData.getMesh().materialForEachVertex[0]))
-            return;
-        // Create an empty display list
-        int id = GlStateManager.glGenLists(1);
-        // Start the compilation of the list, this will fill the list with every vertex rendered onwards
-        GlStateManager.glNewList(id, GL11.GL_COMPILE);
-        //Do render
-        renderCPU(model, "Default", "Default");
-        // Finish the compilation process
-        GlStateManager.glEndList();
-        //log.info("Compile default list for part "+getName()+" of "+model.getLocation());
-        modelDisplayList.put((byte) 0, id);
+        createList(null, null, model, false);
     }
 
     public void render(ObjModelRenderer model, byte textureDataId) {
@@ -117,8 +93,7 @@ public class ObjObjectRenderer {
             GlStateManager.color(1, 1, 1);
         } else
             GlStateManager.callList(modelDisplayList.get(textureDataId));
-        //GL11.glBindTexture(GL11.GL_TEXTURE_2D, Minecraft.getMinecraft().getTextureMapBlocks().getGlTextureId());
-        GlStateManager.bindTexture(Minecraft.getMinecraft().getTextureMapBlocks().getGlTextureId()); //Confirm to mc that used texture has changed
+        GlStateManager.bindTexture(Minecraft.getMinecraft().getTextureMapBlocks().getGlTextureId());
     }
 
     private String getExistingTexture(Material material, String first, String second) {
@@ -131,21 +106,11 @@ public class ObjObjectRenderer {
     private void renderCPU(ObjModelRenderer model, String useDefault, String textureName) {
         //Reset bind texture
         startDrawing();
-        // The model is not compiled, this shouldn't happen if the setup is correct
         Tessellator tess = Tessellator.getInstance();
-        // WorldRenderer renderer = tess.getWorldRenderer();
         BufferBuilder renderer = tess.getBuffer();
-        Vector3f color = new Vector3f(1, 1, 1);
-        float alpha = 1f;
-        Material bind = null;
-        bind = objObjectData.getMesh().materialForEachVertex[0];
-        String bindName = getExistingTexture(objObjectData.getMesh().materialForEachVertex[0], textureName, useDefault);
-        MaterialTexture materialMultipleTextures = objObjectData.getMesh().materialForEachVertex[0].diffuseTexture.get(bindName);
-        if (materialMultipleTextures != null) {
-            bindTexture(objObjectData.getMesh().materialForEachVertex[0].diffuseTexture.get(bindName).getGlTextureId());
-        } else {
-            log.error("Failed to load Default texture of " + objObjectData.getName() + " in " + model.getLocation() + " in material " + objObjectData.getMesh().materialForEachVertex[0].getName());
-        }
+        Material bind = objObjectData.getMesh().materialForEachVertex[0];
+        drawTexture(model, bind, "Default", textureName, false);
+
         int[] indices = objObjectData.getMesh().indices;
         Vertex[] vertices = objObjectData.getMesh().vertices;
 
@@ -158,18 +123,12 @@ public class ObjObjectRenderer {
             Vertex v1 = vertices[i1];
             Vertex v2 = vertices[i2];
 
-            if (isMaterialValid(model, objObjectData.getMesh().materialForEachVertex[i / 3]) && objObjectData.getMesh().materialForEachVertex[i / 3] != bind) {
-                bind = objObjectData.getMesh().materialForEachVertex[i / 3];
-                //System.out.println("Bind material " + bind.getName() + " " + bind.diffuseTex + " " + bind.diffuseTexture + " in model " + this.filename);
+            Material materialToBind = objObjectData.getMesh().materialForEachVertex[i / 3];
+            if (isMaterialValid(model, materialToBind) && materialToBind != bind) {
+                bind = materialToBind;
                 if (drawing)
                     tess.draw();
-                bindName = getExistingTexture(objObjectData.getMesh().materialForEachVertex[i / 3], textureName, useDefault);
-                materialMultipleTextures = objObjectData.getMesh().materialForEachVertex[i / 3].diffuseTexture.get(bindName);
-                if (materialMultipleTextures != null) {
-                    bindTexture(objObjectData.getMesh().materialForEachVertex[i / 3].diffuseTexture.get(bindName).getGlTextureId());
-                } else {
-                    log.error("Failed to load Default texture of " + objObjectData.getName() + " in " + model.getLocation() + " in material " + objObjectData.getMesh().materialForEachVertex[i / 3].getName());
-                }
+                drawTexture(model, materialToBind, "Default", textureName, false);
                 begining = true;
             }
             if (begining) {
@@ -184,6 +143,37 @@ public class ObjObjectRenderer {
         if (drawing)
             tess.draw();
         bindTexture(Minecraft.getMinecraft().getTextureMapBlocks().getGlTextureId());
+    }
+
+    private void drawTexture(ObjModelRenderer model, Material material, String useDefault, String textureName, boolean areVbosEnabled) {
+        String bindName = getExistingTexture(material, textureName, useDefault);
+        MaterialTexture materialMultipleTextures = material.diffuseTexture.get(bindName);
+        if (materialMultipleTextures != null) {
+            if(areVbosEnabled) {
+                Minecraft.getMinecraft().getTextureManager().bindTexture(materialMultipleTextures.getPath());
+            } else {
+                bindTexture(materialMultipleTextures.getGlTextureId());
+            }
+        } else {
+            log.error("Failed to load Default texture of " + objObjectData.getName() + " in " + model.getLocation() + " in material " + material.getName());
+        }
+    }
+
+    /**
+     * Binds the texture, if not already bound
+     */
+    private static void bindTexture(int id) {
+        if (id != bindTexture) {
+            GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+            bindTexture = id;
+        }
+    }
+
+    /**
+     * Resets memory of last bind texture
+     */
+    private static void startDrawing() {
+        bindTexture = -1;
     }
 
     private boolean isMaterialValid(ObjModelRenderer model, Material material) {

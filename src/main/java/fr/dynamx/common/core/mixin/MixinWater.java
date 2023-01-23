9b4c73501c54cad279a9fb2f5f731edbb34e0472
Yanis Boudiaf <yansam2003@gmail.com>
2023-01-23 14:45:02 +0100

    Added comments to our stencil buffer related code

--- src/main/java/fr/dynamx/common/core/mixin/MixinWater.java
+++ src/main/java/fr/dynamx/common/core/mixin/MixinWater.java
@@ -8,16 +8,26 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
+/**
+ * First step for our stencil test
+ * In this step we execute the basic things to use our stencil buffer. Then, we make everything pass the stencil test
+ * @see MixinRenderManager for the second step
+ */
 @Mixin(value = EntityRenderer.class, remap = MixinChunk.REMAP)
 public abstract class MixinWater {
+
     @Inject(method = "renderWorldPass(IFJ)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;renderEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/client/renderer/culling/ICamera;F)V"))
     private void preRenderEntities(int pass, float partialTicks, long finishTimeNano, CallbackInfo ci) {
+        //Before minecraft's opaque pass
         if (MinecraftForgeClient.getRenderPass() == 0) {
+            //Clears the buffer
             GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
+            //Enables the stencil test
             GL11.glEnable(GL11.GL_STENCIL_TEST);
+            //We always want all the entities to pass the test
             GL11.glStencilFunc(GL11.GL_ALWAYS, 1, 0xFF);
+            //Disables writing to the buffer
             GL11.glStencilMask(0x00);
         }
     }

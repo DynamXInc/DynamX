af91b3293e905dbddaacf59a2bd08bf2df7814c1
Aym <aymericb5@gmail.com>
2023-01-29 00:41:35 +0100

    Fix compatibility with sponge player move events

--- src/main/java/fr/dynamx/common/core/mixin/MixinEntity.java
+++ src/main/java/fr/dynamx/common/core/mixin/MixinEntity.java
@@ -1,22 +1,103 @@
 package fr.dynamx.common.core.mixin;
 
-import fr.dynamx.common.core.AABBCollisionHandler;
+import fr.dynamx.common.DynamXContext;
+import fr.dynamx.utils.debug.Profiler;
+import fr.dynamx.utils.optimization.Vector3fPool;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MoverType;
+import net.minecraft.util.math.AxisAlignedBB;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Patches the world raytrace to raytrace on dynamx blocks
  */
 @Mixin(value = Entity.class, priority = 800)
 public abstract class MixinEntity {
+
+    @Shadow
+    public abstract void setEntityBoundingBox(AxisAlignedBB bb);
+
+    @Shadow
+    public abstract AxisAlignedBB getEntityBoundingBox();
+
+    private double x1, y1, z1;
+
     /**
      * @author Aym'
-     * @reason Because DynamX
+     * @reason Collisions with DynamX entities and blocks
      */
-    @Overwrite
-    public void move(MoverType type, double x, double y, double z) {
-        AABBCollisionHandler.vanillaMove((Entity) (Object) this, type, x, y, z);
+    @Inject(method = "move",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/math/AxisAlignedBB;",
+                    shift = At.Shift.AFTER, ordinal = 5))
+    private void move(MoverType type, double x, double y, double z, CallbackInfo ci) {
+        AxisAlignedBB axisalignedbb = getEntityBoundingBox();
+        Vector3fPool.openPool();
+        Profiler.get().start(Profiler.Profiles.ENTITY_COLLISION);
+        double[] data = DynamXContext.getCollisionHandler().handleCollisionWithBulletEntities((Entity) (Object) this, x, y, z);
+        Profiler.get().end(Profiler.Profiles.ENTITY_COLLISION);
+        Vector3fPool.closePool();
+        x1 = data[0];
+        y1 = data[1];
+        z1 = data[2];
+        setEntityBoundingBox(axisalignedbb.offset(x1, y1, z1));
+    }
+
+    @ModifyVariable(method = "move", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/math/AxisAlignedBB;",
+            shift = At.Shift.AFTER, ordinal = 5), argsOnly = true, ordinal = 0)
+    private double injectX1(double x) {
+        return 0;
+    }
+
+    @ModifyVariable(method = "move", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/math/AxisAlignedBB;",
+            shift = At.Shift.AFTER, ordinal = 5), argsOnly = true, ordinal = 1)
+    private double injectY1(double y) {
+        return 0;
+    }
+
+    @ModifyVariable(method = "move", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/entity/Entity;getEntityBoundingBox()Lnet/minecraft/util/math/AxisAlignedBB;",
+            shift = At.Shift.AFTER, ordinal = 5), argsOnly = true, ordinal = 2)
+    private double injectZ1(double z) {
+        return 0;
+    }
+
+    @ModifyVariable(method = "move", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/entity/Entity;onGround:Z",
+            opcode = Opcodes.GETFIELD, ordinal = 1), argsOnly = true, ordinal = 0)
+    private double injectX(double x) {
+        return x1;
+    }
+
+    @ModifyVariable(method = "move", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/entity/Entity;onGround:Z",
+            opcode = Opcodes.GETFIELD, ordinal = 1), argsOnly = true, ordinal = 1)
+    private double injectY(double y) {
+        return y1;
+    }
+
+    @ModifyVariable(method = "move",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;onGround:Z",
+                    opcode = Opcodes.GETFIELD, ordinal = 1), argsOnly = true, ordinal = 2)
+    private double injectZ(double z) {
+        return z1;
+    }
+
+    /*@Redirect(method = "move", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;onGround:Z", opcode = Opcodes.GETFIELD, ordinal = 1))
+    private boolean injected(Entity instance) {
+        //System.out.println("RetIS = " + (onGround && !DynamXContext.getCollisionHandler().motionHasChanged()));
+        return onGround && !DynamXContext.getCollisionHandler().motionHasChanged();
+    }*/
+    @ModifyVariable(method = "move", at = @At("STORE"), ordinal = 0)
+    private boolean fixFlag(boolean flag) {
+        return flag && !DynamXContext.getCollisionHandler().motionHasChanged();
     }
 }

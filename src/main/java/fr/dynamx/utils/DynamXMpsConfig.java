2c893b8080fcbdac62da586776271daf65aca756
Aym <aymericb5@gmail.com>
2023-02-16 00:20:14 +0100

    Fixed MPS

--- src/main/java/fr/dynamx/utils/DynamXMpsConfig.java
+++ src/main/java/fr/dynamx/utils/DynamXMpsConfig.java
@@ -3,13 +3,11 @@ package fr.dynamx.utils;
 import fr.aym.mps.core.BasicMpsConfig;
 import fr.aym.mps.utils.ProtectionException;
 
-import java.util.Arrays;
-
 import static fr.dynamx.utils.DynamXConstants.*;
 
 public class DynamXMpsConfig extends BasicMpsConfig {
     public DynamXMpsConfig() {
-        super(VERSION, MPS_KEY, null, MPS_URL, MPS_AUX_URL,
+        super(VERSION, MPS_KEY, null, MPS_URL, new String[]{MPS_AUX_URL},
                 new String[]{DYNAMX_CERT}, MPS_STARTER, 0, 0, 0, new byte[]{});
     }
 

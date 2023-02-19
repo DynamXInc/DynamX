83f0a3f4c99bf873b32f27c735a8680bcb32645e
aym <aymericb5@gmail.com>
2023-02-20 00:02:17 +0100

    Fix crash with schematica

--- src/main/java/fr/dynamx/client/ClientProxy.java
+++ src/main/java/fr/dynamx/client/ClientProxy.java
@@ -161,7 +161,7 @@ public class ClientProxy extends CommonProxy implements ISelectiveResourceReload
     }
 
     @Override
-    public void providePhysicsWorld(World world) {
+    public void initPhysicsWorld(World world) {
         if (DynamXContext.getPhysicsWorldPerDimensionMap().containsKey(world.provider.getDimension()))
             throw new IllegalStateException("Physics world of " + world + " is already loaded ! World: " + DynamXContext.getPhysicsWorldPerDimensionMap().get(world.provider.getDimension()));
         DynamXContext.getPhysicsWorldPerDimensionMap().put(world.provider.getDimension(), new BuiltinThreadedPhysicsWorld(world, !ClientEventHandler.MC.isSingleplayer()));

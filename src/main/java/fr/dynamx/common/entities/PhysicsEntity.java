2c28be71eac819e289c6b1e082f1912e973cbae4
aym <aymericb5@gmail.com>
2023-02-21 23:16:03 +0100

    Add DoUdpTimeOut config option

--- src/main/java/fr/dynamx/common/entities/PhysicsEntity.java
+++ src/main/java/fr/dynamx/common/entities/PhysicsEntity.java
@@ -20,6 +20,7 @@ import fr.dynamx.common.physics.entities.AbstractEntityPhysicsHandler;
 import fr.dynamx.common.physics.joints.EntityJointsHandler;
 import fr.dynamx.common.physics.player.WalkingOnPlayerController;
 import fr.dynamx.common.physics.terrain.PhysicsEntityTerrainLoader;
+import fr.dynamx.utils.DynamXConfig;
 import fr.dynamx.utils.DynamXUtils;
 import fr.dynamx.utils.PhysicsEntityException;
 import fr.dynamx.utils.debug.Profiler;
@@ -528,7 +529,7 @@ public abstract class PhysicsEntity<T extends AbstractEntityPhysicsHandler<?, ?>
 
     public void printReport() {
         IPhysicsWorld physicsWorld = DynamXContext.getPhysicsWorld(world);
-        if(terrainCache != null && usesPhysicsWorld && physicsWorld != null) {
+        if(terrainCache != null && usesPhysicsWorld && physicsWorld != null && DynamXConfig.enableDebugTerrainManager) {
             System.out.println("============");
             System.out.println("TERRAIN REPORT - " + this);
             terrainCache.printReport(physicsWorld.getTerrainManager());

d4fb62d61fbbf65eb52226070151f1b2ce5ffa03
Yanis Boudiaf <yansam2003@gmail.com>
2023-07-11 15:40:50 +0200

    Changed DynamX and Acs-Guis version

--- src/main/java/fr/dynamx/utils/DynamXConstants.java
+++ src/main/java/fr/dynamx/utils/DynamXConstants.java
@@ -7,12 +7,12 @@ public class DynamXConstants
 {
     public static final String NAME = "DynamX";
     public static final String ID = "dynamxmod";
-    public static final String VERSION = "4.0.0-beta-19";
+    public static final String VERSION = "4.0.0-beta-20";
     public static final String VERSION_TYPE = "Beta";
     public static final String RES_DIR_NAME = "DynamX";
 
     public static final String ACS_GUIS_BASE_URL = "https://maven.dynamx.fr/artifactory/ACsGuisRepo/fr/aym/acsguis/ACsGuis/%1$s/ACsGuis-%1$s.jar";
-    public static final String DEFAULT_ACSGUIS_VERSION = "1.2.5";
+    public static final String DEFAULT_ACSGUIS_VERSION = "1.2.8";
     public static final String ACSLIBS_REQUIRED_VERSION = "[1.2.2,)";
 
     public static final String LIBBULLET_VERSION = "18.1.0";
